91a24202e4a1a64a5f2d256a75ebcbea0a0978cd
mowuj <mowuj92@gmail.com>
2022-04-07 21:12:51 +0600

    email registration

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDoY5FvLw9PRNPUaP-dXtTW2RP_78F2oVk",
+  authDomain: "email-password-auth-1e394.firebaseapp.com",
+  projectId: "email-password-auth-1e394",
+  storageBucket: "email-password-auth-1e394.appspot.com",
+  messagingSenderId: "576537466089",
+  appId: "1:576537466089:web:e5d694fc8455089fe80c78"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
+import 'bootstrap/dist/css/bootstrap.min.css';
+const container =document.getElementById('root')
+const root=ReactDOMClient.createRoot(container)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
